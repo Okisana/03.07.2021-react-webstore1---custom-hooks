b52adad77f0e25ad2483f083844bf01e841f92a7
Oksana <77448138+Okisana@users.noreply.github.com>
2021-09-29 09:30:12 +0300

    map available products

--- src/views/Category.js
+++ src/views/Category.js
@@ -1,18 +1,17 @@
 import { NavLink } from "react-router-dom";
 import "./Style2.css";
+import { productData } from "./productData";
+import { useCartContext } from "./CartContext";
 
-import product1 from "../images/y3.jpg";
-import product2 from "../images/shop/dRed1.jpg";
-import product3 from "../images/shop/dress3.jpg";
-import product4 from "../images/ddress4.jpeg";
-import product5 from "../images/shop/dStripe.jpg";
-import product6 from "../images/shop/dress1.jpg";
 import img from "../images/shop/slider-2.jpg";
 
 function Category() {
+  const { addNewProduct } = useCartContext();
+
   const modalWindow = () => {
     alert("Your item was added to cart");
   };
+
   return (
     <div>
       <div class="container">
@@ -72,168 +71,43 @@ function Category() {
           <div class="container">
             <div class="row text-center d-flex justify-content-center mt-3 cardSize">
               <div class="row text-center d-flex justify-content-center mt-3">
-                <div class="col-3 card border-warning text-center mx-3 ">
-                  <img
-                    class=" img-fluid cardImage2"
-                    src={product1}
-                    alt="product image"
-                  />
-                  <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <NavLink to="/product">
-                        <h5 class="card-text mb-2 ">Summer yellow dress</h5>
-                      </NavLink>
-                      <h4 class="card-title mb-3">€19.99</h4>
-                    </div>
-                    <div class="d-grid gap">
-                      <button
-                        onClick={modalWindow}
-                        class=" btn btn-danger fw-bold"
-                        type="button"
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-3 card border-warning text-center mx-3 ">
-                  <img
-                    class=" img-fluid cardImage2"
-                    src={product2}
-                    alt="product image"
-                  />
-                  <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <NavLink to="/product">
-                        <h5 class="card-text mb-2">Openwork dress</h5>
-                      </NavLink>
-                      <h4 class="card-title mb-3">€34.99</h4>
-                    </div>
-
-                    <div class="d-grid gap ">
-                      <button
-                        onClick={modalWindow}
-                        class="btn btn-danger fw-bold"
-                        type="button "
-                      >
-                        Add to Cart
-                      </button>
+                {productData.map((product) => (
+                  <div class="col-3 card border-warning text-center mx-3 mt-3">
+                    <img
+                      class=" img-fluid cardImage2"
+                      src={product.image}
+                      alt="product image"
+                    />
+                    <div class="card-body d-flex flex-column justify-content-between">
+                      <div>
+                        <NavLink to="/product">
+                          <h5 class="card-text mb-2 ">{product.name}</h5>
+                        </NavLink>
+                        <h4 class="card-title mb-3">
+                          ({product.price}).toFixed(2)
+                        </h4>
+                      </div>
+                      <div class="d-grid gap">
+                        <button
+                          onClick={() => {
+                            addNewProduct(product);
+                            modalWindow();
+                          }}
+                          class=" btn btn-danger fw-bold"
+                          type="button"
+                        >
+                          Add to Cart
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                <div class="col-3 card border-warning text-center mx-3 ">
-                  <img
-                    class=" img-fluid cardImage2"
-                    src={product3}
-                    alt="product image"
-                  />
-                  <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <NavLink to="/product">
-                        <h5 class="card-text mb-2">Midi dress</h5>
-                      </NavLink>
-                      <h4 class="card-title mb-3">€42.99</h4>
-                    </div>
-
-                    <div class="d-grid gap ">
-                      <button
-                        onClick={modalWindow}
-                        class="btn btn-danger fw-bold"
-                        type="button "
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row text-center d-flex justify-content-center mt-3">
-                <div class="col-3 card border-warning text-center mx-3">
-                  <img
-                    class=" img-fluid cardImage2"
-                    src={product4}
-                    alt="product image"
-                  />
-                  <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <NavLink to="/product">
-                        <h5 class="card-text mb-2">Red dress</h5>
-                      </NavLink>
-                      <h4 class="card-title mb-3">€27.99</h4>
-                    </div>
-                    <div class="d-grid gap">
-                      <button
-                        onClick={modalWindow}
-                        class=" btn btn-danger fw-bold"
-                        type="button "
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-3 card border-warning text-center mx-3 ">
-                  <img
-                    class=" img-fluid cardImage2"
-                    src={product5}
-                    alt="product image"
-                  />
-                  <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <NavLink to="/product">
-                        <h5 class="card-text mb-2">Striped dress</h5>
-                      </NavLink>
-                      <h4 class="card-title mb-3">€40.99</h4>
-                    </div>
-
-                    <div class="d-grid gap ">
-                      <button
-                        onClick={modalWindow}
-                        class="btn btn-danger fw-bold"
-                        type="button "
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-3 card border-warning text-center mx-3 ">
-                  <img
-                    class=" img-fluid cardImage2"
-                    src={product6}
-                    alt="product image"
-                  />
-                  <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <NavLink to="/product">
-                        <h5 class="card-text mb-2">Classic blue dress</h5>
-                      </NavLink>
-                      <h4 class="card-title mb-3">€22.99</h4>
-                    </div>
-
-                    <div class="d-grid gap ">
-                      <button
-                        onClick={modalWindow}
-                        class="btn btn-danger fw-bold"
-                        type="button "
-                      >
-                        Add to Cart
-                      </button>
-                    </div>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
+            <div class="my-5"></div>
+
+            <div class="empty"></div>
           </div>
-
-          <div class="my-5"></div>
-
-          <div class="empty"></div>
         </div>
       </div>
     </div>
