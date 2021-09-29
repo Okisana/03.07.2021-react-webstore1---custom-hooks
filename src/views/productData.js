970404ea3c7df68a65599465729f64fcd6fe89c6
Oksana <77448138+Okisana@users.noreply.github.com>
2021-09-29 09:28:29 +0300

    add data

---
+++ src/views/productData.js
@@ -0,0 +1,45 @@
+import product7 from "../images/shop/dMidi.jpg";
+import product8 from "../images/shop/dBlack.jpg";
+import product9 from "../images/shop/dStripe.jpg";
+import product2 from "../images/shop/dRed1.jpg";
+import product3 from "../images/shop/dress3.jpg";
+import product6 from "../images/shop/dress1.jpg";
+
+export const productData = [
+  {
+    id: 0,
+    name: "Summer floral dress",
+    price: 20.0,
+    image: product7,
+  },
+  {
+    id: 1,
+    name: "Black dress",
+    price: 30.0,
+    image: product8,
+  },
+  {
+    id: 2,
+    name: "Stripe dress",
+    price: 15.0,
+    image: product9,
+  },
+  {
+    id: 3,
+    name: "Red summer dress",
+    price: 20.0,
+    image: product2,
+  },
+  {
+    id: 4,
+    name: "Blue summer dress",
+    price: 30.0,
+    image: product6,
+  },
+  {
+    id: 5,
+    name: "Midi dress",
+    price: 45.5,
+    image: product3,
+  },
+];
